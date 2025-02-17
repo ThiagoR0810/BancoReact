aca4e337bddafbca34be1106c764bc9f39a68957
Thiago Ribeiro <thiago.ribeiroc.0810@gmail.com>
2025-02-17 17:24:20 -0300

    creating the name input

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+    };
+
+    this.getName = this.getName.bind(this);
+  }
+
+  getName(nameText) {
+    if (nameText.length > 0) {
+      this.setState({name: nameText});
+    } else {
+      this.setState({name: ''});
+    }
+  }
+
+  render() { 
+    return (
+      <View style={styles.container}>
+        <View style={styles.titleView}>
+          <Text style={styles.title}>Welcome to React Bank!</Text>
+        </View>
+
+        <View style={styles.nameView}>
+          <Text style={styles.nameText}>Name: </Text>
+          <TextInput
+            style={styles.nameInput}
+            placeholder="Type your name"
+            underlineColorAndroid="transparent"
+            onChangeText={this.getName}
+          />
+        </View>
+        
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20
+  },
+  titleView:{
+    alignItems: 'center'
+  },
+  nameView:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginLeft: 10
+  },
+  nameText:{
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  nameInput:{
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 10,
+    fontSize: 15,
+    padding: 10
+  }
 });
+
+export default App;
